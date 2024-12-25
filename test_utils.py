6f05382d1a351c75f0992b1cf1e89a5342176cfd
dasNessie <mail@dasnessie.de>
2024-12-25 23:06:37 +0100

    feat: :sparkles: Add utils function to decide whether to keep a pairing

--- test_utils.py
+++ test_utils.py
@@ -1,10 +1,23 @@
-from utils import _generate_pairing
+from utils import _accept_pairing, _generate_pairing
 
 
 def test_generate_pairing():
-    participants = ['a', 'b', 'c']
+    participants = ["a", "b", "c"]
     assert _generate_pairing(participants) in [
         {"a": "b", "b": "c", "c": "a"},
         {"a": "c", "b": "a", "c": "b"},
     ]
-    assert participants == ['a', 'b', 'c']
+    assert participants == ["a", "b", "c"]
+
+
+def test_accept_pairing():
+    pairing = {"a": "b", "b": "c", "c": "a"}
+    pairs_with_probability = {("a", "b"): 0.5}
+
+    selected_count = 0
+    for i in range(100):
+        if _accept_pairing(pairs_with_probability, pairing):
+            selected_count += 1
+
+    assert selected_count >= 38
+    assert selected_count <= 62
