db31738361a54a69c7a8e72b219adfb50c2c1f6b
dasNessie <mail@dasnessie.de>
2024-12-26 20:51:40 +0100

    refactor: :recycle: Unify pairing format to set of tuples

--- test_utils.py
+++ test_utils.py
@@ -5,14 +5,14 @@ from utils import _accept_pairing, _generate_pairing
 def test_generate_pairing():
     participants = ["a", "b", "c"]
     assert _generate_pairing(participants) in [
-        {"a": "b", "b": "c", "c": "a"},
-        {"a": "c", "b": "a", "c": "b"},
+        {("a", "b"), ("b", "c"), ("c", "a")},
+        {("a", "c"), ("b", "a"), ("c", "b")},
     ]
     assert participants == ["a", "b", "c"]
 
 
 def test_accept_pairing():
-    pairing = {"a": "b", "b": "c", "c": "a"}
+    pairing = {("a", "b"), ("b", "c"), ("c", "a")}
     pairs_with_probability = {("a", "b"): 0.5}
 
     random.seed(6740)
--- utils.py
+++ utils.py
@@ -2,39 +2,43 @@ from copy import deepcopy
 from random import random, shuffle
 
 
-def _generate_pairing(participants: list[str]) -> dict[str, str]:
+def _generate_pairing(participants: list[str]) -> set[tuple[str, str]]:
     """Generate a single pairing from a list of participants
 
     Args:
         participants (list[str]): list of participant names
 
     Returns:
-        dict[str, str]: dict mapping each participant (as key) to someone to gift (as value)
+        set[tuple[str, str]]: set of tuples mapping each participant (first tuple entry) to someone to gift (second tuple entry)
     """
     shuffled_participants = deepcopy(participants)
     shuffle(shuffled_participants)
-    result = {}
+    result = set()
     for i in range(len(shuffled_participants)):
-        result[shuffled_participants[i]] = shuffled_participants[
-            (i + 1) % len(shuffled_participants)
-        ]
+        result.add(
+            (
+                shuffled_participants[i],
+                shuffled_participants[(i + 1) % len(shuffled_participants)],
+            )
+        )
     return result
 
 
 def _accept_pairing(
-    pairs_with_probabilities: dict[tuple[str, str], float], pairing: dict
+    pairs_with_probabilities: dict[tuple[str, str], float],
+    pairing: set[tuple[str, str]],
 ) -> bool:
     """Decide if pairing should be accepted given probabilities for specific pairs
 
     Args:
         pairs_with_probabilities (dict[tuple[str, str], float]): Dict containing tuple of (giver, giftee) as key and probability as value
-        pairing (dict): a pairing, eg generated with _generate_pairing
+        pairing (set[tuple[str, str]]): a pairing asset of tuples of (giver, giftee), eg generated with _generate_pairing
 
     Returns:
         bool: true if pairing should be accepted
     """
     restricted_pairs = pairs_with_probabilities.keys()
-    for gifter, giftee in pairing.items():
+    for (gifter, giftee) in pairing:
         if (gifter, giftee) in restricted_pairs:
             if random() > pairs_with_probabilities[(gifter, giftee)]:
                 return False
