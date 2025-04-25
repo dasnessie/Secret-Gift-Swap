d1edf5a2eaedb814514e97d202ddf7bc5c4d02bf
dasnessie <mail@dasnessie.de>
2025-04-25 19:16:27 +0200

    feat: :necktie: Update utils to use new data structure

--- test_utils.py
+++ test_utils.py
@@ -1,20 +1,33 @@
 import random
 import pytest
+from constraints import Constraint, Constraints
+from matching import Matching
+from participant import Participant
 from utils import get_pairing_with_probabilities, _generate_pairing, _accept_pairing
 
 
 def test_generate_pairing():
-    participants = ["a", "b", "c"]
+    pa = Participant(name="a")
+    pb = Participant(name="b")
+    pc = Participant(name="c")
+    participants = [pa, pb, pc]
     assert _generate_pairing(participants) in [
-        {("a", "b"), ("b", "c"), ("c", "a")},
-        {("a", "c"), ("b", "a"), ("c", "b")},
+        Matching({pa: pb, pb: pc, pc: pa}),
+        Matching({pa: pc, pb: pa, pc: pb}),
     ]
-    assert participants == ["a", "b", "c"]
+    assert participants == [
+        pa,
+        pb,
+        pc,
+    ]
 
 
 def test_accept_pairing():
-    pairing = {("a", "b"), ("b", "c"), ("c", "a")}
-    pairs_with_probability = {("a", "b"): 0.5}
+    pa = Participant(name="a")
+    pb = Participant(name="b")
+    pc = Participant(name="c")
+    pairing = Matching({pa: pb, pb: pc, pc: pa})
+    pairs_with_probability = Constraints([Constraint(pa, pb, "3_past_exchange")])
 
     random.seed(6740)
 
@@ -23,46 +36,60 @@ def test_accept_pairing():
         if _accept_pairing(pairs_with_probability, pairing):
             selected_count += 1
 
-    assert selected_count == 47
+    assert selected_count == 41
 
 
 def test_get_pairing_with_probabilities():
-    participants = ["a", "b", "c"]
-    assert get_pairing_with_probabilities(participants) in [
-        {("a", "b"), ("b", "c"), ("c", "a")},
-        {("a", "c"), ("b", "a"), ("c", "b")},
+    pa = Participant(name="a")
+    pb = Participant(name="b")
+    pc = Participant(name="c")
+    pd = Participant(name="d")
+    participants = [pa, pb, pc]
+    assert get_pairing_with_probabilities(participants).matching in [
+        {pa: pb, pb: pc, pc: pa},
+        {pa: pc, pb: pa, pc: pb},
     ]
 
     assert get_pairing_with_probabilities(
-        participants=["a", "b", "c", "d"],
-        pairs_with_probabilities={
-            ("a", "b"): 0,
-            ("a", "c"): 0,
-            ("b", "c"): 0,
-            ("b", "d"): 0,
-            ("c", "a"): 0,
-            ("c", "d"): 0,
-            ("d", "a"): 0,
-            ("d", "b"): 0,
-        },
-    ) == {("a", "d"), ("b", "a"), ("c", "b"), ("d", "c")}
+        participants=[pa, pb, pc, pd],
+        pairs_with_probabilities=Constraints(
+            [
+                Constraint(pa, pb, "1_past_exchange"),
+                Constraint(pa, pc, "1_past_exchange"),
+                Constraint(pb, pc, "1_past_exchange"),
+                Constraint(pb, pd, "1_past_exchange"),
+                Constraint(pc, pa, "1_past_exchange"),
+                Constraint(pc, pd, "1_past_exchange"),
+                Constraint(pd, pa, "1_past_exchange"),
+                Constraint(pd, pb, "1_past_exchange"),
+            ]
+        ),
+    ).matching == {pa: pd, pb: pa, pc: pb, pd: pc}
 
     random.seed(6851)
 
     assert get_pairing_with_probabilities(
-        participants=["a", "b", "c", "d"],
-        pairs_with_probabilities={
-            ("a", "b"): 0.5,
-            ("b", "d"): 0.5,
-            ("c", "d"): 0.5,
-            ("d", "a"): 0.5,
-        },
-    ) == {("c", "d"), ("a", "c"), ("d", "b"), ("b", "a")}
+        participants=[pa, pb, pc, pd],
+        pairs_with_probabilities=Constraints(
+            [
+                Constraint(pa, pb, "3_past_exchange"),
+                Constraint(pb, pd, "3_past_exchange"),
+                Constraint(pc, pd, "3_past_exchange"),
+                Constraint(pd, pa, "3_past_exchange"),
+            ]
+        ),
+    ).matching == {pc: pd, pa: pc, pd: pb, pb: pa}
 
     with pytest.raises(ValueError):
         get_pairing_with_probabilities(
-            participants=["a", "b", "c", "d"],
-            pairs_with_probabilities={("a", "b"): 0, ("a", "c"): 0, ("a", "d"): 0},
+            participants=[pa, pb, pc, pd],
+            pairs_with_probabilities=Constraints(
+                [
+                    Constraint(pa, pb, "1_past_exchange"),
+                    Constraint(pa, pc, "1_past_exchange"),
+                    Constraint(pa, pd, "1_past_exchange"),
+                ]
+            ),
         )
 
     random.seed(6851)
@@ -70,11 +97,13 @@ def test_get_pairing_with_probabilities():
     with pytest.warns(
         UserWarning, match="Could not generate a pairing with given constraints"
     ):
+        cs = Constraints(
+            [
+                Constraint(pa, pb, "2_past_exchange"),
+                Constraint(pa, pc, "2_past_exchange"),
+                Constraint(pa, pd, "2_past_exchange"),
+            ]
+        )
         assert get_pairing_with_probabilities(
-            participants=["a", "b", "c", "d"],
-            pairs_with_probabilities={
-                ("a", "b"): 0.001,
-                ("a", "c"): 0.001,
-                ("a", "d"): 0.001,
-            },
-        ) == {("b", "a"), ("d", "c"), ("a", "d"), ("c", "b")}
+            participants=[pa, pb, pc, pd], pairs_with_probabilities=cs, retries=1
+        ).matching == {pb: pa, pd: pb, pa: pc, pc: pd}
--- utils.py
+++ utils.py
@@ -2,78 +2,89 @@ from copy import deepcopy
 from random import random, shuffle
 import warnings
 
+from constraints import Constraint, Constraints
+from matching import Matching
+from participant import Participant
 
-def _generate_pairing(participants: list[str]) -> set[tuple[str, str]]:
+
+def _generate_pairing(participants: list[Participant]) -> Matching:
     """Generate a single pairing from a list of participants
 
     Args:
-        participants (list[str]): list of participant names
+        participants (list[Participant]): list of participants
 
     Returns:
-        set[tuple[str, str]]: set of tuples mapping each participant (first tuple entry) to someone to gift (second tuple entry)
+        Matching: Matching of participants
     """
     shuffled_participants = deepcopy(participants)
     shuffle(shuffled_participants)
-    result = set()
+    result = {}
     for i in range(len(shuffled_participants)):
-        result.add(
-            (
-                shuffled_participants[i],
-                shuffled_participants[(i + 1) % len(shuffled_participants)],
-            )
-        )
-    return result
+        result[shuffled_participants[i]] = shuffled_participants[
+            (i + 1) % len(shuffled_participants)
+        ]
+    return Matching(result)
 
 
 def _accept_pairing(
-    pairs_with_probabilities: dict[tuple[str, str], float],
-    pairing: set[tuple[str, str]],
+    pairs_with_probabilities: Constraints,
+    pairing: Matching,
+    probability_multiplier: float = 1.0,
 ) -> bool:
     """Decide if pairing should be accepted given probabilities for specific pairs
 
     Args:
-        pairs_with_probabilities (dict[tuple[str, str], float]): Dict containing tuple of (giver, giftee) as key and probability as value
-        pairing (set[tuple[str, str]]): a pairing asset of tuples of (giver, giftee), eg generated with _generate_pairing
+        pairs_with_probabilities (Constraints): Constraints
+        pairing (Matching): a pairing, eg generated with _generate_pairing
 
     Returns:
         bool: true if pairing should be accepted
     """
-    restricted_pairs = pairs_with_probabilities.keys()
-    for gifter, giftee in pairing:
-        if (gifter, giftee) in restricted_pairs:
-            if random() > pairs_with_probabilities[(gifter, giftee)]:
+    for giver, giftee in pairing:
+        if (giver, giftee) in pairs_with_probabilities.restricted_pairs():
+            if (
+                random()
+                > pairs_with_probabilities[(giver, giftee)] * probability_multiplier
+            ):
                 return False
     return True
 
 
 def get_pairing_with_probabilities(
-    participants: list[str], pairs_with_probabilities: dict[tuple[str, str], float] = {}
-) -> set[tuple[str, str]]:
-    """_summary_
+    participants: list[Participant],
+    pairs_with_probabilities: Constraints = Constraints([]),
+    retries: int = 100,
+) -> Matching:
+    """Generate one pairing, using probabilities
 
     Args:
-        participants (list[str]): list of participant names
-        pairs_with_probabilities (dict[tuple[str, str], float], optional): Dict containing tuple of (giver, giftee) as key and probability as value. Defaults to {}.
+        participants (list[Participant]): list of participants
+        pairs_with_probabilities (COnstraints, optional): Constraints to respect. Defaults to empty set of constraints.
 
     Raises:
         ValueError: No suitable pairing found
 
     Returns:
-        set[tuple[str, str]]: set of tuples mapping each participant (first tuple entry) to someone to gift (second tuple entry)
+        Matching: A matching
     """
+    probability_multiplier = 1.0
     for i in range(5):
-        for i in range(100):
+        for i in range(retries):
             pairing = _generate_pairing(participants)
-            if _accept_pairing(pairs_with_probabilities, pairing):
+            if _accept_pairing(
+                pairs_with_probabilities, pairing, probability_multiplier
+            ):
                 return pairing
         if (
-            all(value == 0 for value in pairs_with_probabilities.values())
+            all(
+                value == 0
+                for value in pairs_with_probabilities.values_as_probabilities()
+            )
             or len(pairs_with_probabilities) == 0
         ):
             break  # increasing the probability would not help here, so we skip that
         warnings.warn(
             "Could not generate a pairing with given constraints (I tried 100 times)! Increasing probabilities and trying again..."
         )
-        for pair, probability in pairs_with_probabilities.items():
-            pairs_with_probabilities[pair] == probability * 1.2
+        probability_multiplier = probability_multiplier * 1.2
     raise ValueError("Could not generate a pairing with these constraints!")
