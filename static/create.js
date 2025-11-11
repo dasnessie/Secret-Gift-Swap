0d87937edbc6b3990d6692141ff7b0115c09aea8
dasnessie <mail@dasnessie.de>
2025-11-11 17:40:04 +0100

    feat: :sparkles: Add participant input fields by pressing the enter key

--- static/create.js
+++ static/create.js
@@ -1,8 +1,24 @@
-document.getElementById("add-participant").addEventListener("click", () => {
+function addParticipantField() {
   const participants = document.getElementById("participants");
   const newParticipantLine = participants.lastElementChild.cloneNode(true);
   newParticipantLine.getElementsByTagName("input")[0].value = "";
   participants.appendChild(newParticipantLine);
+  newParticipantLine.firstElementChild.focus();
+}
+
+document
+  .getElementById("add-participant")
+  .addEventListener("click", addParticipantField);
+
+document.getElementById("participants").addEventListener("keydown", (e) => {
+  if (e.key != "Enter") {
+    return;
+  }
+  if (!e.target.classList.contains("participant")) {
+    return;
+  }
+  e.preventDefault();
+  addParticipantField();
 });
 
 document.getElementById("participants").addEventListener("click", (e) => {
